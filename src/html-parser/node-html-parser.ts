89ea9aaa4657242af161ae808ffa9bb1d368830a
Timur Mukhametyanov <37434920+Canfus@users.noreply.github.com>
2025-04-16 21:09:35 +0300

    Update node-html-parser.ts

--- src/html-parser/node-html-parser.ts
+++ src/html-parser/node-html-parser.ts
@@ -1,45 +1,22 @@
-import { HTMLParser, type ParserOptions } from './htmlParser'
-
-export class NodeHTMLParser extends HTMLParser {
-  static async parseFromString(html: string): Promise<Document> {
-    const { JSDOM } = await import('jsdom')
-
-    if (!JSDOM) {
-      throw new Error('Cannot find jsdom in dependencies. Try to install jsdom. pnpm add jsdom')
-    }
-
+import { BaseHTMLParser, type ParserOptions } from './baseHtmlParser'
+class NodeHTMLParserError extends Error {
+  constructor(
+    message: string,
+    public originalError?: Error
+  ) {
+    super(message)
+    this.name = 'NodeHTMLParserError'
+  }
+}
+export class NodeHTMLParser extends BaseHTMLParser {
+  constructor(options?: ParserOptions) {
+    super(options)
+  }
+  async parseFromString(html: string): Promise<Document> {
+    const { JSDOM } = await import('jsdom').catch((error) => {
+      throw new NodeHTMLParserError('Cannot find jsdom in dependencies. Try to install jsdom', error as Error)
+    })
     const dom = new JSDOM(html)
-
     return dom.window.document
-  }
-
-  static async parse(
-    html: string,
-    as: 'string' | 'node',
-    options?: ParserOptions
-  ): Promise<{ stringNodes: string[]; nodes: Node[] }> {
-    const { includeBody } = options || {}
-
-    const stringNodes: string[] = []
-    const nodes: Node[] = []
-    const doc = await this.parseFromString(html)
-
-    const elements = as === 'string' ? stringNodes : nodes
-
-    if (includeBody) {
-      this.traverse(doc.body, as, elements, options)
-    } else {
-      for (const child of doc.body.childNodes) {
-        this.traverse(child, as, elements, options)
-      }
-    }
-
-    return { stringNodes, nodes }
-  }
-
-  static async countElements(html: string, options?: ParserOptions): Promise<number> {
-    const { stringNodes } = await this.parse(html, 'string', options)
-
-    return stringNodes.length
   }
 }
