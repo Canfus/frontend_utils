dc14189fdac928800b338bafa375f528eabf3640
Timur Mukhametyanov <37434920+Canfus@users.noreply.github.com>
2025-04-16 21:29:52 +0300

    Update interface.ts

--- src/use-breadcrumbs/interface.ts
+++ src/use-breadcrumbs/interface.ts
@@ -9,24 +9,33 @@ type PathMatcher = (breadcrumb: Breadcrumb) => string
 
 export type UseBreadcrumbsOptions = {
   /**
+   * включить главную страницу
+   * @default true
+   */
+  rootEnabled?: boolean
+  /**
    * переопределяет отображаемый лейбл хлебной крошки
+   * @default undefined
    */
   matcher?: PathMatcher
   /**
    * указывает начало хлебных крошек
    * @typeParam string - path name
    * @typeParam number - path index
+   * @default undefined
    */
   startsWith?: number | string
   /**
    * указывает конец хлебных крошек
    * @typeParam string - path name
    * @typeParam number - path index
+   * @default undefined
    */
   endsWith?: number | string
   /**
    * фильтрация хлебных крошек
    * @param breadcrumb
+   * @default undefined
    */
   filter?: (breadcrumb: Breadcrumb) => boolean
 }
